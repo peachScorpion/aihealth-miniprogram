ff29efff4a730a24df18dad5a8850697bcf4b884
peachScorpion <411496785@qq.com>
2026-09-14 18:58:04 +0800

    Upload miniprogram/components/progress-bar/progress-bar.js

---
+++ miniprogram/components/progress-bar/progress-bar.js
@@ -0,0 +1,3 @@
+Component({
+  properties: { percent: { type: Number, value: 0 }, current: { type: Number, value: 0 }, total: { type: Number, value: 0 }, showText: { type: Boolean, value: true } }
+})
